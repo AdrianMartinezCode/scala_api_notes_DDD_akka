cb290da8a7dba5be03e37ef97916c22c2c0bf806
adrian <amartinezm1994@gmail.com>
2019-05-04 18:49:51 +0200

    Changes

---
+++ src/main/scala/domain/Domain.scala
@@ -0,0 +1,17 @@
+import domain.{Note, User}
+
+trait RestMessage
+
+case class GetUsersWithNotes(usersWithNotes: List[User]) extends RestMessage
+case class UsersWithNotes(users: Seq[User]) extends RestMessage
+
+case class GetNotes(notes: List [Note]) extends RestMessage
+case class Notes(notes: Seq[Note]) extends RestMessage
+
+object SuccessfulOperation
+final case class Validation(msg: String)
+final case class Error(msg: String)
+final case class NotExists(msg: String)
+
+case object UserOverflowException extends Exception("Exc")
+case object NoteOverflowException extends Exception("")
